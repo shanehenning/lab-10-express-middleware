6874f416ce1f1324ebffaf4dabaa94e5cae30d6b
Shane Henning <shanerhenning@gmail.com>
2016-07-27 10:55:16 -0700

    AppErrors abound

---
+++ lab-shane/app_error.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let AppError = module.exports = exports = function(message, error, responseMessage){
+  this.message = message;
+  this.error = error;
+  this.responseMessage = responseMessage;
+};
+
+AppError.error204 = function(msg){
+  return new AppError(msg, 204, 'No Content');
+};
+
+AppError.error400 = function(msg){
+  return new AppError(msg, 400, 'Bad Request');
+};
+
+AppError.error404 = function(msg){
+  return new AppError(msg, 404, 'File Not Found');
+};
+
+AppError.error500 = function(msg){
+  return new AppError(msg, 500, 'Internal Server Error');
+};
+
+AppError.instAppError = function(error){
+  return error instanceof AppError;
+};
